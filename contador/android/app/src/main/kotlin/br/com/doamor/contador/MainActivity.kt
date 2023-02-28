9fa5066092c74d062e66f4a8f722bf1d8f64a5d1
DiogoRede <diogorede2021@gmail.com>
2023-02-28 15:24:57 -0300

    Primeiro projeto, um contador de controle para estabelecimento

---
+++ contador/android/app/src/main/kotlin/br/com/doamor/contador/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.doamor.contador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ contador/lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int contador = 0;
+
+  void decrement() {
+    setState(() {
+      contador--;
+    });
+  }
+
+  void increment() {
+    setState(() {
+      contador++;
+    });
+  }
+
+  bool get isEmpyt => contador == 0;
+  bool get isFull => contador == 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/homeBackground.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isFull ? 'Lotado' : 'Pode Entrar!',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: isFull ? Colors.red : Colors.blueGrey,
+                  fontWeight: FontWeight.w900),
+            ),
+            Text(
+              '$contador',
+              style: TextStyle(
+                fontSize: 100,
+                color: isFull ? Colors.red : Colors.blueGrey,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        isEmpyt ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(90, 90),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: isEmpyt ? null : decrement,
+                  child: const Text(
+                    'Saiu',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 30,
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        isFull ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(90, 90),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: isFull ? null : increment,
+                  child: const Text(
+                    'Entrou',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
